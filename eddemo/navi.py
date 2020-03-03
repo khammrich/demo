26ac443828ccc2135c46c44babd65866f236ab11
Kyle <kyle@temperedlab.com>
2020-03-03 13:12:13 -0800

    eddemo pycurl

---
+++ eddemo/navi.py
@@ -0,0 +1,7 @@
+nav = [
+    {'name': 'Home', 'url': '/'},
+    {'name': 'About', 'url': 'about'},
+    {'name': 'pycURL', 'url': 'pythoncurl'},
+    {'name': 'Contact', 'url': 'contact'},
+    {'name': 'Login', 'url': 'userauth'}
+]
